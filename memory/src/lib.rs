66792b6cabd0ef9832bd7999e7f211ec44af13b5
Adam Snaider <adsnaider@gmail.com>
2022-03-13 15:11:26 -0700

    [memory] Setup memory crate.

---
+++ memory/src/lib.rs
@@ -0,0 +1,17 @@
+//! Memory allocation and paging utilities.
+#![cfg_attr(not(test), no_std)]
+#![deny(absolute_paths_not_starting_with_crate)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(unsafe_op_in_unsafe_fn)]
+
+#[cfg(target_os = "linux")]
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
