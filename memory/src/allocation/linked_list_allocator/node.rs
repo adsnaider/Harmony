f4c3e171b2f58b5fee08577d811e4150f791c796
Adam Snaider <adsnaider@gmail.com>
2022-03-19 15:20:45 -0700

    [allocator] Added documentation for the Node struct.

--- memory/src/allocation/linked_list_allocator/node.rs
+++ memory/src/allocation/linked_list_allocator/node.rs
@@ -30,6 +30,7 @@ impl Node {
         }
     }
 
+    /// Constructs a sentinel node that has null empty buffer.
     pub fn sentinel() -> Self {
         Self {
             next: None,
@@ -38,6 +39,7 @@ impl Node {
         }
     }
 
+    /// Returns true if the node is a sentinel.
     pub fn is_sentinel(&self) -> bool {
         self.buffer.start().is_null() && self.buffer.is_empty()
     }
@@ -58,6 +60,7 @@ impl Node {
         self.next
     }
 
+    /// Sets the `next` pointer.
     pub fn set_next(&mut self, next: Option<NonNull<Node>>) {
         self.next = next;
     }
@@ -67,24 +70,41 @@ impl Node {
         self.prev
     }
 
+    /// Sets the `prev` pointer.
     pub fn set_prev(&mut self, prev: Option<NonNull<Node>>) {
         self.prev = prev;
     }
 
+    /// Returns the `buffer` corresponding to this node.
     pub fn buffer(&self) -> MemoryRegion {
         self.buffer
     }
 
+    /// Shrinks the buffer to `size` and returns the leftover region. If `size` is larger than the
+    /// current buffer, it returns None and the buffer won't change.
     pub fn shrink_to(&mut self, size: usize) -> Option<MemoryRegion> {
         let leftover;
         (self.buffer, leftover) = self.buffer.partition(size)?;
         Some(leftover)
     }
 
-    pub fn grow(&mut self, by: usize) {
-        self.buffer = MemoryRegion::from_addr_and_size(self.buffer.start(), self.buffer.len() + by);
+    /// Grows the buffer by `count` bytes.
+    pub fn grow(&mut self, count: usize) {
+        self.buffer =
+            MemoryRegion::from_addr_and_size(self.buffer.start(), self.buffer.len() + count);
     }
 
+    /// Returns the `SplitNodeResult` for the given layout.
+    ///
+    /// You can think of this function as a hypothetical of what would happen if we tried to use
+    /// this node to allocate for a given layout. The result can either be a `Misfit` (alignment or
+    /// size constraints), a `Partition(at)` which indicates that the node should be shrunk to the
+    /// returned size, or a `Hijack`, which indicates that the node should be used to its entierty
+    /// to allocate the layout.
+    ///
+    /// The caller may assume that on a `Partition` result, the provided partition point will be
+    /// correct to allow for a new Node to be created on the leftover space and that the node will
+    /// have a non-empty buffer.
     pub fn split_for_layout(&self, layout: Layout) -> SplitNodeResult {
         if !self.buffer.is_aligned(layout.align()) {
             return SplitNodeResult::Misfit;
@@ -104,6 +124,7 @@ impl Node {
         }
     }
 
+    /// Doubly links two nodes together.
     pub fn link(left: &mut Self, right: &mut Self) {
         left.next = Some(right.into());
         right.prev = Some(left.into());
