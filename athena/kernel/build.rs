6d5927fbb82f26ef69cf810bb1f2662dd0477c58
Adam Snaider <adsnaider@gmail.com>
2023-04-23 23:38:44 -0700

    Fix kernel build.rs

--- athena/kernel/build.rs
+++ athena/kernel/build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     Command::new("make")
-        .args(["-C", "programs/", "all"])
+        .args(["-C", "programs/"])
         .status()
         .expect("Failed to run makefile for init binaries");
 }
