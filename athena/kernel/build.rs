726ad3f7307b0ad274e1b27435f6aa45851e3e8e
Adam Snaider <adsnaider@gmail.com>
2023-04-24 00:40:57 -0700

    Improve build.rs

--- athena/kernel/build.rs
+++ athena/kernel/build.rs
@@ -2,8 +2,9 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=programs/");
-    Command::new("make")
+    let status = Command::new("make")
         .args(["-C", "programs/"])
         .status()
         .expect("Failed to run makefile for init binaries");
+    assert!(status.success(), "Make error");
 }
