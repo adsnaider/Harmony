bad1efc5cb7ae78a637288fc20178f1847eac206
Adam Snaider <adsnaider@gmail.com>
2023-06-04 16:13:36 -0700

    Better scheduler.

--- athena/kernel/src/sched.rs
+++ athena/kernel/src/sched.rs
@@ -1,148 +1,156 @@
 //! The kernel scheduler.
 
-use alloc::boxed::Box;
 use alloc::collections::VecDeque;
-use alloc::vec::Vec;
-use core::cell::UnsafeCell;
+use core::cell::{RefCell, UnsafeCell};
 use core::fmt::Debug;
 use core::sync::atomic::{AtomicU64, Ordering};
 
 use critical_section::Mutex;
+use hashbrown::{HashMap, HashSet};
 use once_cell::sync::OnceCell;
 
 use crate::arch::context::Context;
-
-#[derive(Debug)]
-struct SchedContext {
-    context: Context,
-    id: u64,
-}
-
-impl SchedContext {
-    pub fn new(context: crate::arch::context::Context) -> Self {
-        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
-        let id = NEXT_ID.fetch_add(1, Ordering::Relaxed);
-        Self { context, id }
-    }
-}
-
-impl From<Context> for SchedContext {
-    fn from(value: crate::arch::context::Context) -> Self {
-        Self::new(value)
-    }
-}
+use crate::dbg;
 
 /// The kernel scheduler.
-#[allow(missing_debug_implementations)]
+#[derive(Debug)]
 pub struct Scheduler {
-    readyq: VecDeque<SchedContext>,
-    blocked: Vec<(SchedContext, Box<dyn Fn() -> bool + Send>)>,
-    current: SchedContext,
+    readyq: RefCell<VecDeque<u64>>,
+    blocked: RefCell<HashSet<u64>>,
+    current: RefCell<Option<u64>>,
+    tasks: RefCell<HashMap<u64, UnsafeCell<Context>>>,
 }
 
-static SCHEDULER: OnceCell<Mutex<UnsafeCell<Scheduler>>> = OnceCell::new();
+static SCHEDULER: OnceCell<Mutex<Scheduler>> = OnceCell::new();
 
 /// Initializes the scheduler.
 pub fn init() {
     SCHEDULER
-        .set(Mutex::new(UnsafeCell::new(Scheduler::new(Context::main()))))
+        .set(Mutex::new(Scheduler::new(Context::main())))
         .unwrap();
 }
 
 /// Pushes a new task to be scheduled.
-pub fn push(task: crate::arch::context::Context) {
-    critical_section::with(|cs| unsafe {
-        (*SCHEDULER.get().unwrap().borrow(cs).get()).push(task);
-    })
+pub fn push(task: crate::arch::context::Context) -> u64 {
+    critical_section::with(|cs| SCHEDULER.get().unwrap().borrow(cs).push(task))
 }
 
 /// Performs a context switch.
 pub fn switch() {
-    critical_section::with(|cs| unsafe {
-        (*SCHEDULER.get().unwrap().borrow(cs).get()).switch();
-    })
+    critical_section::with(|cs| SCHEDULER.get().unwrap().borrow(cs).switch())
 }
 
 /// Terminates the currently running task and schedules the next one
 pub fn exit() -> ! {
-    critical_section::with(|cs| unsafe {
-        (*SCHEDULER.get().unwrap().borrow(cs).get()).exit();
-    })
+    critical_section::with(|cs| SCHEDULER.get().unwrap().borrow(cs).exit())
+}
+
+/// Blocks the current context until a wake up is received.
+pub fn block() {
+    critical_section::with(|cs| SCHEDULER.get().unwrap().borrow(cs).block())
+}
+
+/// Wakes up a given context (if blocked).
+pub fn wakeup(tid: u64) {
+    critical_section::with(|cs| SCHEDULER.get().unwrap().borrow(cs).wakeup(tid))
 }
 
 impl Scheduler {
+    fn next_tid() -> u64 {
+        static NEXT_TID: AtomicU64 = AtomicU64::new(0);
+        NEXT_TID.fetch_add(1, Ordering::Relaxed)
+    }
+
     /// Creates an empty scheduler.
     pub fn new(current: Context) -> Self {
-        let mut this = Self {
-            readyq: VecDeque::new(),
-            current: current.into(),
-            blocked: Vec::new(),
-        };
-        this.push(Context::kthread(Self::looper));
-        this
+        let tid = Self::next_tid();
+        let mut tasks = HashMap::new();
+        tasks.try_insert(tid, UnsafeCell::new(current)).unwrap();
+
+        Self {
+            readyq: RefCell::new(VecDeque::new()),
+            current: RefCell::new(Some(tid)),
+            blocked: RefCell::new(HashSet::new()),
+            tasks: RefCell::new(tasks),
+        }
     }
 
     /// Pushes a new task to the scheduler.
-    pub fn push(&mut self, task: Context) {
-        self.readyq.push_back(task.into());
+    pub fn push(&self, task: Context) -> u64 {
+        let tid = Self::next_tid();
+        self.tasks
+            .borrow_mut()
+            .try_insert(tid, UnsafeCell::new(task))
+            .unwrap();
+        self.readyq.borrow_mut().push_back(tid);
+        dbg!(tid)
     }
 
     /// Schedules the next task to run.
     ///
     /// Upon a follow up switch, the function will return back to its caller.
-    pub fn switch(&mut self) {
+    pub fn switch(&self) {
         let Some(next) = self
-            .readyq
+            .readyq.borrow_mut()
             .pop_front() else {
             // Nothing else to run, back to caller.
             return;
         };
-        let previous = core::mem::replace(&mut self.current, next);
-        self.readyq.push_back(previous);
-        let previous = self.readyq.back_mut().unwrap();
+        let previous = self.current.borrow_mut().replace(next).unwrap();
+        self.readyq.borrow_mut().push_back(previous);
         // SAFETY: This is super awkward but hopefully safe.
         // * It's probably not cool to keep references that need to live after the switch, so we use raw pointers.
         // * The pointers only become references before the switch, not after.
         // * When the switch comes back to us (on a further restore, we don't have any more references around).
+        unsafe { self.switch_to(next, previous) }
+    }
+
+    unsafe fn switch_to(&self, next: u64, previous: u64) {
+        assert!(next != previous);
         unsafe {
-            // This function will restore interrupts
-            Context::switch(&self.current.context, &mut previous.context);
+            let previous = self.tasks.borrow()[&previous].get();
+            let next = self.tasks.borrow()[&next].get();
+
+            Context::switch(next, previous);
+        }
+    }
+
+    unsafe fn jump_to(&self, next: u64) -> ! {
+        unsafe {
+            let next = self.tasks.borrow()[&next].get();
+            Context::jump(next);
         }
     }
 
     /// Terminates the currently running task and schedules the next one
-    pub fn exit(&mut self) -> ! {
-        let next = self.readyq.pop_front().expect("Looper should be ready");
-        self.current = next;
+    pub fn exit(&self) -> ! {
+        let next = self
+            .readyq
+            .borrow_mut()
+            .pop_front()
+            .expect("Looper should be ready");
+        let previous = self.current.borrow_mut().replace(next).unwrap();
+        self.tasks.borrow_mut().remove(&previous).unwrap();
         unsafe {
-            Context::jump(&self.current.context);
+            self.jump_to(next);
         }
     }
 
     /// Blocks the current process
-    pub fn block<F>(&mut self, ready: F)
-    where
-        F: Fn() -> bool + Send + 'static,
-    {
-        if !ready() {
-            let next = self.readyq.pop_front().expect("Looper should be ready");
-            let previous = core::mem::replace(&mut self.current, next);
-            self.blocked.push((previous, Box::new(ready)));
-            let previous = &mut self.blocked.last_mut().unwrap().0;
-            unsafe {
-                Context::switch(&self.current.context, &mut previous.context);
-            }
-        }
+    pub fn block(&self) {
+        let next = self.readyq.borrow_mut().pop_front().unwrap();
+        let previous = self.current.borrow_mut().replace(next).unwrap();
+        assert!(self.blocked.borrow_mut().insert(previous));
+        dbg!(&self.blocked);
+        unsafe { self.switch_to(next, previous) }
     }
 
-    fn looper() {
-        loop {
-            crate::arch::inst::hlt();
+    /// Awake a blocked context.
+    pub fn wakeup(&self, id: u64) {
+        dbg!(id);
+        dbg!(&self.blocked);
+        if dbg!(self.blocked.borrow_mut().remove(&id)) {
+            self.readyq.borrow_mut().push_back(id);
         }
-    }
-
-    /// Poll blocked process and awake if ready.
-    pub fn poll_blocked(&mut self, id: u64) -> bool {
-        todo!();
     }
 }
