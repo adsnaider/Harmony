88b712f61e964079eb627054b9e4387208eb734c
Adam Snaider <adsnaider@gmail.com>
2024-05-13 13:52:55 -0400

    Read memory map from limine

--- athena/kernel/src/main.rs
+++ athena/kernel/src/main.rs
@@ -9,17 +9,57 @@
 )]
 #![cfg_attr(target_arch = "x86_64", feature(abi_x86_interrupt))]
 
-#[cfg(test)]
-mod testing;
+use limine::request::{HhdmRequest, MemoryMapRequest};
+use limine::BaseRevision;
+use sync::cell::AtomicLazyCell;
+
+use crate::arch::interrupts;
 
 pub mod arch;
 pub mod syscall;
 
+#[cfg(test)]
+mod testing;
+
 mod serial;
 
+pub static PMO: AtomicLazyCell<usize> = AtomicLazyCell::new(|| {
+    #[used]
+    static HHDM: HhdmRequest = HhdmRequest::new();
+
+    let pmo = HHDM
+        .get_response()
+        .expect("Missing Higher-half direct mapping response from limine")
+        .offset();
+    // PMO must be on the higher half
+    assert!(pmo > 0xFFFF_8000_0000_0000);
+    pmo as usize
+});
+
 pub fn init() {
+    #[used]
+    static BASE_REVISION: BaseRevision = BaseRevision::with_revision(1);
+
+    #[used]
+    static mut MEMORY_MAP: MemoryMapRequest = MemoryMapRequest::new();
+    interrupts::disable();
+
     serial::init();
+    assert!(
+        BASE_REVISION.is_supported(),
+        "Limine revision not supported"
+    );
+
     arch::init();
+
+    log::info!("Got physical memory offset from limine at {:#X}", *PMO);
+
+    let _memory_map = unsafe {
+        MEMORY_MAP
+            .get_response_mut()
+            .expect("Missing memory map from Limine")
+            .entries_mut()
+    };
 }
 
 #[cfg(all(target_os = "none", not(test)))]
@@ -33,5 +73,6 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
 #[cfg(not(test))]
 #[no_mangle]
 extern "C" fn kmain() -> ! {
+    init();
     loop {}
 }
