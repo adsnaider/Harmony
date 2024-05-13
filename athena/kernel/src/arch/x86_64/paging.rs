b67597d09f976dc7197b4897561a3eeda0a4f56a
Adam Snaider <adsnaider@gmail.com>
2024-05-13 15:09:19 -0400

    Add multi-frame allocation to bump allocator

--- athena/kernel/src/arch/x86_64/paging.rs
+++ athena/kernel/src/arch/x86_64/paging.rs
@@ -1,4 +1,5 @@
 pub const PAGE_SIZE: usize = 4096;
+pub const FRAME_SIZE: u64 = 4096;
 
 pub mod frames;
 pub use frames::RawFrame;
--- athena/kernel/src/bump_allocator.rs
+++ athena/kernel/src/bump_allocator.rs
@@ -1,7 +1,8 @@
 use limine::memory_map::{Entry, EntryType};
 use sync::cell::AtomicLazyCell;
 
-use crate::arch::paging::RawFrame;
+use crate::arch::paging::{RawFrame, FRAME_SIZE};
+use crate::MemoryMap;
 pub static BOOT_ALLOCATION: AtomicLazyCell<EntryType> = AtomicLazyCell::new(|| EntryType::from(8));
 pub struct BumpAllocator {
     memory_map: &'static mut [&'static mut Entry],
@@ -9,7 +10,7 @@ pub struct BumpAllocator {
 }
 
 impl BumpAllocator {
-    pub fn new(memory_map: &'static mut [&'static mut Entry]) -> Self {
+    pub fn new(memory_map: MemoryMap) -> Self {
         Self {
             memory_map,
             index: 0,
@@ -17,19 +18,42 @@ impl BumpAllocator {
     }
 
     pub fn alloc_frame(&mut self) -> Option<RawFrame> {
-        const PAGE_SIZE: u64 = crate::arch::paging::PAGE_SIZE as u64;
         let frame = loop {
             let entry = self.memory_map.get_mut(self.index)?;
-            assert!(entry.length % PAGE_SIZE as u64 == 0);
+            assert!(entry.length % FRAME_SIZE == 0);
             if entry.entry_type == EntryType::USABLE && entry.length > 0 {
                 let start_address = entry.base;
-                entry.base += PAGE_SIZE;
-                entry.length -= PAGE_SIZE;
+                entry.base += FRAME_SIZE;
+                entry.length -= FRAME_SIZE;
                 break RawFrame::from_start_address(start_address);
             }
             self.index += 1;
         };
         Some(frame)
+    }
+
+    pub fn alloc_frames(&mut self, count: usize) -> Option<u64> {
+        let requested_length = count as u64 * FRAME_SIZE;
+        let start_address = loop {
+            let entry = self.memory_map.get_mut(self.index)?;
+            assert!(entry.length % FRAME_SIZE == 0);
+            if entry.entry_type == EntryType::USABLE && entry.length >= requested_length {
+                let start_address = entry.base;
+                entry.base += requested_length;
+                entry.length -= requested_length;
+                break start_address;
+            }
+            self.index += 1;
+        };
+        Some(start_address)
+    }
+
+    pub fn into_memory_map(self) -> &'static mut [&'static mut Entry] {
+        self.memory_map
+    }
+
+    pub fn memory_map(&mut self) -> &mut [&'static mut Entry] {
+        self.memory_map
     }
 }
 
@@ -39,26 +63,25 @@ mod tests {
 
     #[test_case]
     fn allocation_test() {
-        const PAGE_SIZE: u64 = crate::arch::paging::PAGE_SIZE as u64;
         static mut TEST_MAP: [&mut Entry; 4] = [
             &mut Entry {
                 base: 0,
-                length: PAGE_SIZE * 2,
+                length: FRAME_SIZE * 2,
                 entry_type: EntryType::USABLE,
             },
             &mut Entry {
-                base: PAGE_SIZE * 4,
-                length: PAGE_SIZE,
+                base: FRAME_SIZE * 4,
+                length: FRAME_SIZE,
                 entry_type: EntryType::USABLE,
             },
             &mut Entry {
-                base: PAGE_SIZE * 5,
-                length: PAGE_SIZE,
+                base: FRAME_SIZE * 5,
+                length: FRAME_SIZE,
                 entry_type: EntryType::RESERVED,
             },
             &mut Entry {
-                base: PAGE_SIZE * 6,
-                length: PAGE_SIZE,
+                base: FRAME_SIZE * 6,
+                length: FRAME_SIZE,
                 entry_type: EntryType::USABLE,
             },
         ];
@@ -66,9 +89,45 @@ mod tests {
         // SAFETY: Mutable access is unique.
         #[allow(static_mut_refs)]
         let mut allocator = BumpAllocator::new(unsafe { &mut TEST_MAP });
-        for expected_frame in [0, PAGE_SIZE, PAGE_SIZE * 4, PAGE_SIZE * 6] {
+        for expected_frame in [0, FRAME_SIZE, FRAME_SIZE * 4, FRAME_SIZE * 6] {
             let expected_frame = RawFrame::from_start_address(expected_frame);
             let frame = allocator.alloc_frame().unwrap();
+            assert_eq!(frame, expected_frame);
+        }
+
+        assert!(allocator.alloc_frame().is_none())
+    }
+
+    #[test_case]
+    fn multi_allocation_test() {
+        static mut TEST_MAP: [&mut Entry; 4] = [
+            &mut Entry {
+                base: 0,
+                length: FRAME_SIZE * 2,
+                entry_type: EntryType::USABLE,
+            },
+            &mut Entry {
+                base: FRAME_SIZE * 4,
+                length: FRAME_SIZE,
+                entry_type: EntryType::USABLE,
+            },
+            &mut Entry {
+                base: FRAME_SIZE * 5,
+                length: FRAME_SIZE,
+                entry_type: EntryType::RESERVED,
+            },
+            &mut Entry {
+                base: FRAME_SIZE * 6,
+                length: FRAME_SIZE * 2,
+                entry_type: EntryType::USABLE,
+            },
+        ];
+
+        // SAFETY: Mutable access is unique.
+        #[allow(static_mut_refs)]
+        let mut allocator = BumpAllocator::new(unsafe { &mut TEST_MAP });
+        for expected_frame in [0, FRAME_SIZE * 6] {
+            let frame = allocator.alloc_frames(2).unwrap();
             assert_eq!(frame, expected_frame);
         }
 
