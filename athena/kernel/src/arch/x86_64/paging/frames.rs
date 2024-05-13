8aacf7914d1c9b66d250ad1db13e8e1e017772dd
Adam Snaider <adsnaider@gmail.com>
2024-05-13 15:28:44 -0400

    Add physical address type

--- athena/kernel/src/arch/x86_64/paging/frames.rs
+++ athena/kernel/src/arch/x86_64/paging/frames.rs
@@ -1,30 +1,30 @@
 //! Physical memory frames for the x86-64 architecture
 
-use super::PAGE_SIZE;
+use super::{PhysAddr, FRAME_SIZE};
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(transparent)]
 pub struct RawFrame {
-    base: u64,
+    base: PhysAddr,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum InvalidFrame {
-    BadAddress,
-    UnalignedAddress,
-}
+pub struct UnalignedAddress;
 
 impl RawFrame {
-    pub fn from_start_address(base: u64) -> Self {
+    pub fn from_start_address(base: PhysAddr) -> Self {
         Self::try_from_start_address(base).unwrap()
     }
 
-    pub fn try_from_start_address(base: u64) -> Result<Self, InvalidFrame> {
-        if base % PAGE_SIZE as u64 != 0 {
-            return Err(InvalidFrame::UnalignedAddress);
-        }
-        if base % (1 << 52) != base {
-            return Err(InvalidFrame::BadAddress);
+    pub fn try_from_start_address(base: PhysAddr) -> Result<Self, UnalignedAddress> {
+        if base.as_u64() % FRAME_SIZE != 0 {
+            return Err(UnalignedAddress);
         }
         Ok(Self { base })
+    }
+
+    pub fn within_frame(addr: PhysAddr) -> Self {
+        let base = PhysAddr::new(addr.as_u64() % FRAME_SIZE);
+        Self { base }
     }
 }
