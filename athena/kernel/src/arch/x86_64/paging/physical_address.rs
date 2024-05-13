1ade2c237792ff4a56b997cdaf7c6ac89779f071
Adam Snaider <adsnaider@gmail.com>
2024-05-13 15:43:04 -0400

    Add virtual memory addresing

--- athena/kernel/src/arch/x86_64/paging/physical_address.rs
+++ athena/kernel/src/arch/x86_64/paging/physical_address.rs
@@ -1,32 +1,39 @@
+use super::VirtAddr;
+use crate::PMO;
+
 #[repr(transparent)]
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct PhysAddr {
-    addr: u64,
-}
+pub struct PhysAddr(u64);
 
 #[derive(Debug)]
 pub struct BadAddress;
 
 impl PhysAddr {
-    pub fn new(addr: u64) -> Self {
-        Self::try_new(addr).unwrap()
+    pub const fn new(addr: u64) -> Self {
+        match Self::try_new(addr) {
+            Ok(addr) => addr,
+            Err(_) => panic!("Invalid Physical Address: Must be up to 52 bits"),
+        }
     }
 
-    pub fn try_new(addr: u64) -> Result<Self, BadAddress> {
-        if Self::new_truncate(addr).addr == addr {
-            Ok(Self { addr })
+    pub const fn try_new(addr: u64) -> Result<Self, BadAddress> {
+        if Self::new_truncate(addr).0 == addr {
+            Ok(Self(addr))
         } else {
             Err(BadAddress)
         }
     }
 
-    pub fn new_truncate(addr: u64) -> Self {
-        Self {
-            addr: addr % (1 << 52),
-        }
+    pub const fn new_truncate(addr: u64) -> Self {
+        Self(addr % (1 << 52))
     }
 
-    pub fn as_u64(&self) -> u64 {
-        self.addr
+    pub const fn as_u64(&self) -> u64 {
+        self.0
+    }
+
+    pub fn to_virtual(self) -> VirtAddr {
+        let virt = PMO.get().as_usize() + self.0 as usize;
+        VirtAddr::new(virt)
     }
 }
