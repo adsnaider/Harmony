7278f7c8ddcfaea029d2930d96958ccbe877b810
Adam Snaider <adsnaider@gmail.com>
2024-08-10 01:17:14 -0400

    WIP: booter: start initializing memory manager

--- harmony/userspace/booter/src/main.rs
+++ harmony/userspace/booter/src/main.rs
@@ -8,6 +8,7 @@ use kapi::raw::CapId;
 use kapi::userspace::cap_managment::{FrameAllocator, SelfCapabilityManager};
 use kapi::userspace::structures::PhysFrame;
 use kapi::userspace::Booter;
+use tar_no_std::TarArchiveRef;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -36,15 +37,33 @@ impl FrameAllocator for &'_ FrameBumper {
 }
 
 #[no_mangle]
-extern "C" fn _start(lowest_frame: usize) -> ! {
+extern "C" fn _start(lowest_frame: usize, initrd: *const u8, initrd_size: usize) -> ! {
     let resources = Booter::make();
 
     resources.hardware.enable_ports().unwrap();
     serial::init();
 
+    let initrd = unsafe { core::slice::from_raw_parts(initrd, initrd_size) };
+    log::info!(
+        "Jumped to userspace: next_frame: {}, initrd: ({:?}, {})",
+        lowest_frame,
+        initrd.as_ptr(),
+        initrd.len()
+    );
+
     let frames = FrameBumper::new(PhysFrame::new(lowest_frame));
     let _cap_manager =
         SelfCapabilityManager::new_with_start(resources.self_caps, CapId::new(6), &frames);
+
+    let initrd = TarArchiveRef::new(initrd).expect("Bad initramfs");
+    let _memory_manager = initrd
+        .entries()
+        .find(|entry| {
+            entry.filename().as_str().expect("Invalid entry in initrd") == "memory_manager"
+        })
+        .expect("Missing memory_manager from initrd")
+        .data();
+
     log::info!("Initializing user space");
     loop {}
 }
